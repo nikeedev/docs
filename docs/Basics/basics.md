c1741cc2660e38b4362d42dc02110d4bcd16273e
nikeedev <nikita.gon.dev@gmail.com>
2024-02-14 12:31:19 +0100

    up

--- docs/Basics/basics.md
+++
@@ -1,1 +0,0 @@
-# The Basics of creating an addon:
--- publish.ps1
+++ publish.ps1
@@ -1,14 +1,15 @@
-if (Test-Path $env:temp/book) {
-    echo "doesnt exist"
+$temp_exits = Test-Path $env:temp/book
+if ($temp_exits -eq $false) {
+    Write-Output "doesnt exist"
     mkdir $env:temp/book
 }
 
 git worktree add $env:temp/book gh-pages
 mdbook build
-rm -Force $env:temp/book/* # this won't delete the .git directory
-cp -Recurse book/* $env:temp/book/
-cd $env:temp/book
+Remove-Item -Force $env:temp/book/* # this won't delete the .git directory
+Copy-Item -Recurse book/* $env:temp/book/ -Confirm
+Set-Location $env:temp/book
 git add -A
 git commit -m 'deploy new book'
 git push origin gh-pages
-cd -
+Set-Location -
